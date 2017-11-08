56b4df11bfa11fe2d8b75e6d33a9774b311e49ae
Aaron <aa07.mac@gmail.com>
2017-11-08 16:24:07 -0330

    Commented code

--- main.go
+++ main.go
@@ -9,21 +9,24 @@ import (
 )
 
 func main() {
+	//seedin random number to current time ensures it will be reasonably random
 	rand.Seed( time.Now().UTC().UnixNano())
 
+	//create and parse command line flags
 	uptoInt := flag.Int("b", 12, "The Highest B value * 2")
 	flag.Parse()
 
+	//variable i will hold the user input
 	var i int
 
 	for {
-		base := rand.Intn(*uptoInt)
-		b := base * 2
-		c := int(math.Pow(float64(base), 2))
-		fmt.Printf("B=%d c=?", b)
-		fmt.Scan(&i)
+		base := rand.Intn(*uptoInt) //base number to build trinomial
+		b := base * 2 // b value
+		c := int(math.Pow(float64(base), 2)) //c value
+		fmt.Printf("B=%d c=?", b) //ask question
+		fmt.Scan(&i) //get user input
 
-		if i != c {
+		if i != c { //check answer, print if wrong
 			fmt.Printf("Wrong. B=%d C=%d",b, c)
 		}
 	}
